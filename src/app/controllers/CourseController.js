3b8e2cc66e7c1189500a90c735ab1f3c8e1b4810
Hung Nguyen <1119thaihungnguyen@gmail.com>
2022-04-18 21:18:57 +0700

    Course detail page

---
+++ src/app/controllers/CourseController.js
@@ -0,0 +1,15 @@
+const Course = require("./model/Course");
+const { mongooseToObject } = require('../../util/mongoose')
+class CourseController {
+    show(req, res, next) {
+        Course.findOne({ slug: req.params.slug })
+            .then((course) => {
+                res.render('courses/show', { course: mongooseToObject(course) });
+            })
+            .catch(next);
+    }
+}
+
+module.exports = new CourseController();
+
+// const newsController = require('./NewsController');
