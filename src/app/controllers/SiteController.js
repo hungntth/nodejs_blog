c79d7b6c42a9ccd02d6e63ca219330de3b58b06c
Hung Nguyen <1119thaihungnguyen@gmail.com>
2022-04-18 20:27:43 +0700

    Read From DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,18 +1,17 @@
 const Course = require('./model/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose')
 
 
 class SiteController {
 
 
-    index(req, res) {
+    index(req, res, next) {
 
-        Course.find({}, function (err, courses) {
-            if (!err) {
-                res.json(courses);
-                return;
-            }
-            res.status(400).json({ error: "ERROR" });
-        });
+        Course.find({})
+            .then(courses => {
+                res.render('home', { courses: mutipleMongooseToObject(courses) });
+            })
+            .catch(error => next(error))
         // res.render("home");
     }
 
