bb367ef4c982903736b32144ccf6556865e9c54a
Hung Nguyen <1119thaihungnguyen@gmail.com>
2022-04-19 09:40:37 +0700

    Create new course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -8,6 +8,22 @@ class CourseController {
             })
             .catch(next);
     }
+
+    create(req, res, next) {
+        res.render("courses/create")
+    }
+
+    store(req, res, next) {
+        // res.json(req.body)
+        const formData = req.body
+        formData.image = `https://img.youtube.com/vi/${formData.videoID}/sddefault.jpg`;
+        const course = new Course(formData);
+        course.save()
+            .then(() => res.redirect('/'))
+            .catch(error => {
+
+            })
+    }
 }
 
 module.exports = new CourseController();
--- src/app/controllers/model/Course.js
+++ src/app/controllers/model/Course.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug);
+
+
+
 
 const Course = new Schema({
     name: { type: String, maxLenght: 255 },
     description: { type: String, maxLenght: 600 },
     image: { type: String, maxLenght: 255 },
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
+    videoID: { type: String },
+    slug: { type: String, slug: "name", unique: true }
+}, {
+    timestamps: true,
 });
 
 module.exports = mongoose.model('Course', Course);
