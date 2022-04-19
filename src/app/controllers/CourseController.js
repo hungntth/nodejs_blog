78def1360a395a7081faa7cf26b4ce6617a84549
Hung Nguyen <1119thaihungnguyen@gmail.com>
2022-04-19 11:29:40 +0700

    Update course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -24,6 +24,19 @@ class CourseController {
 
             })
     }
+
+    edit(req, res, next) {
+        Course.findById(req.params.id)
+            .then(courses => res.render('courses/edit', {
+                courses: mongooseToObject(courses)
+            }))
+            .catch(next)
+    }
+    update(req, res, next) {
+        Course.updateOne({ _id: req.params.id }, req.body)
+            .then(() => res.redirect('/me/stored/courses'))
+            .catch(next)
+    }
 }
 
 module.exports = new CourseController();
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const port = 3000;
 const morgan = require("morgan");
 const path = require("path");
 const exphbs = require("express-handlebars");
+const methodOverride = require('method-override')
 
 const route = require('./routes');
 const db = require('./config/db/index');
@@ -14,11 +15,15 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.urlencoded());
 app.use(express.json());
+app.use(methodOverride('_method'))
 
 app.use(morgan("combined"));
 
 app.engine("hbs", exphbs({
-  extname: ".hbs"
+  extname: ".hbs",
+  helpers: {
+    sum: (a, b) => a + b,
+  }
 }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources", "views"));
